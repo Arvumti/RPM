faf81140fde92b252d2d803c618fa2cbf8e9dbde
Jesús Valadez <jesus.valadez@arvumti.com>
2014-06-03 16:02:06 -0500

    cpan 3.0.2
    tamaño de los txtbox

--- funciones_db.php
+++ funciones_db.php
@@ -1,7 +1,7 @@
 <?php
 function get_pdo()
 {
-    $pdo = new PDO('mysql:host=localhost;dbname=db_rpm', 'root', 'toor', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"));
+    $pdo = new PDO('mysql:host=localhost;dbname=db_rpm', 'root', 'root', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"));
 	return $pdo;
 }
 
